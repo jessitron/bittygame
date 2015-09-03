e878796e1a6e951ae482bd6b1796c96de832a9ed
Jessica Kerr <jessitron@outpace.com>
2015-09-03 02:56:19 +0000

    Found magic compile trick to return json

--- src/main/scala/com/jessitron/bittygame_web/MyService.scala
+++ src/main/scala/com/jessitron/bittygame_web/MyService.scala
@@ -4,6 +4,9 @@ import akka.actor.Actor
 import spray.routing._
 import spray.http._
 import MediaTypes._
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+import spray.httpx.SprayJsonSupport._
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -26,13 +29,9 @@ trait MyService extends HttpService {
   val myRoute =
     path("game" / Segment / "begin" ) { seg =>
       get {
-        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+        respondWithMediaType(`application/json`) {
           complete {
-            <html>
-              <body>
-                <h1>Say hello to <i>{ seg }</i> on <i>spray-can</i>!</h1>
-              </body>
-            </html>
+            Map("foo" -> "bar", "morefoo" -> "baz")
           }
         }
       }
