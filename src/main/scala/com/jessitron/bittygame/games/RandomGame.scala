faecf4460b645392f7e46a6545c6a3cd7efbb0cb
Jessica Kerr <jessitron@outpace.com>
2015-09-09 02:31:18 +0000

    refactor: side effects out

--- src/main/scala/com/jessitron/bittygame/games/RandomGame.scala
+++ src/main/scala/com/jessitron/bittygame/games/RandomGame.scala
@@ -8,36 +8,50 @@ import org.scalacheck.Gen
 import scala.io.Source
 
 object RandomGame {
+  import Util._
+  lazy val default = new RandomGame(
+    readResource("firstPartOfSentence.txt"),
+    readResource("secondPartOfSentence.txt"),
+    readResource("verbs.txt"),
+    readResource("nouns.txt"))
+}
 
-  lazy val funSentence: Gen[String] = for {
-    firstPart <- Gen.oneOf("You see", "All around you swarm", readResource("firstPartOfSentence.txt") :_*)
-    secondPart <- Gen.oneOf("monkeys staring at you", "a giant spider", readResource("secondPartOfSentence.txt") :_*)
+class RandomGame(firstPartsOfSentence: Seq[String],
+                 secondPartsOfSentence: Seq[String],
+                 nouns: Seq[String],
+                 verbs: Seq[String]) {
+  private val funSentence: Gen[String] = for {
+    firstPart <- Gen.oneOf("You see", "All around you swarm", firstPartsOfSentence :_*)
+    secondPart <- Gen.oneOf("monkeys staring at you", "a giant spider", secondPartsOfSentence  :_*)
   } yield s"$firstPart $secondPart"
 
-  def funMessage(maxSentences: Int): Gen[String] = for {
+  private def funMessage(maxSentences: Int): Gen[String] = for {
     numberOfSentences <- Gen.choose(1, maxSentences)
     sentences <- Gen.listOfN(numberOfSentences, funSentence)
   } yield sentences.mkString(". ")
 
-  def funAction: Gen[PlayerAction] = for {
-    verb <- Gen.oneOf("take", "open", readResource("verbs.txt"))
-    noun <- Gen.oneOf("door", "book", readResource("nouns.txt"))
+  private def funAction: Gen[PlayerAction] = for {
+    verb <- Gen.oneOf("take", "open", verbs)
+    noun <- Gen.oneOf("door", "book", nouns)
     message <- funMessage(3)
   } yield PlayerAction(s"$verb $noun", message)
 
-  def funGameGen(size: Int) : Gen[GameDefinition] = for {
+  private def funGameGen(size: Int) : Gen[GameDefinition] = for {
     welcomeMessage <- funMessage(5)
     actions <- Gen.listOfN(size, funAction)
   } GameDefinition(actions, welcomeMessage)
 
-  def create(): GameDefinition = untilYouGetOne(funGameGen(10).sample)
-
-  def untilYouGetOne[T](how: => Option[T], limit: Int = 1000): T =
+  private def untilYouGetOne[T](how: => Option[T], limit: Int = 1000): T =
     Iterator.continually(how)
       .take(limit)
       .collectFirst{ case Some(x) => x}
       .getOrElse(throw new RuntimeException(s"Didn't get one after $limit tries"))
 
+  def create(): GameDefinition = untilYouGetOne(funGameGen(10).sample)
+
+}
+
+object Util {
 
   def readResource(name: String): Seq[String] = {
     val source = Source.fromURL(this.getClass().getResource(name))
