2d4f34e16f66682fc3d20f8e869d4571ab090d6d
Jessica Kerr <jessitron@outpace.com>
2015-09-09 02:40:42 +0000

    way to generate name of a game

--- src/main/scala/com/jessitron/bittygame/games/RandomGame.scala
+++ src/main/scala/com/jessitron/bittygame/games/RandomGame.scala
@@ -3,23 +3,38 @@ package com.jessitron.bittygame.games
 import java.nio.file.{Path, Files}
 
 import com.jessitron.bittygame.crux.{PlayerAction, GameDefinition}
+import com.jessitron.bittygame.web.ports.GameDefinitionDAO.GameDefinitionKey
 import org.scalacheck.Gen
 
 import scala.io.Source
 
 object RandomGame {
   import Util._
-  lazy val default = new RandomGame(
+  lazy val defaultGameGen = new RandomGame(
     readResource("firstPartOfSentence.txt"),
     readResource("secondPartOfSentence.txt"),
     readResource("verbs.txt"),
     readResource("nouns.txt"))
+  lazy val defaultNamer = new RandomName(readResource("names.txt"))
+
+  def create() = defaultGameGen.create()
+
+  def name(excluding: Seq[String]) = defaultNamer.name(excluding)
+
+}
+
+class RandomName(names: Seq[String]) {
+  val nameGen = Gen.oneOf("pinkiepie", "heidegger", names:_*)
+
+  def name(excluding: Seq[String]): GameDefinitionKey =
+    Util.untilYouGetOne(nameGen.suchThat(!excluding.contains(_)).sample)
 }
 
 class RandomGame(firstPartsOfSentence: Seq[String],
                  secondPartsOfSentence: Seq[String],
                  nouns: Seq[String],
                  verbs: Seq[String]) {
+
   private val funSentence: Gen[String] = for {
     firstPart <- Gen.oneOf("You see", "All around you swarm", firstPartsOfSentence :_*)
     secondPart <- Gen.oneOf("monkeys staring at you", "a giant spider", secondPartsOfSentence  :_*)
@@ -41,17 +56,12 @@ class RandomGame(firstPartsOfSentence: Seq[String],
     actions <- Gen.listOfN(size, funAction)
   } GameDefinition(actions, welcomeMessage)
 
-  private def untilYouGetOne[T](how: => Option[T], limit: Int = 1000): T =
-    Iterator.continually(how)
-      .take(limit)
-      .collectFirst{ case Some(x) => x}
-      .getOrElse(throw new RuntimeException(s"Didn't get one after $limit tries"))
-
-  def create(): GameDefinition = untilYouGetOne(funGameGen(10).sample)
+  def create(): GameDefinition = Util.untilYouGetOne(funGameGen(10).sample)
 
 }
 
 object Util {
+  // these belong in a microlib
 
   def readResource(name: String): Seq[String] = {
     val source = Source.fromURL(this.getClass().getResource(name))
@@ -61,5 +71,11 @@ object Util {
       source.close()
     }
   }
+
+  def untilYouGetOne[T](how: => Option[T], limit: Int = 1000): T =
+    Iterator.continually(how)
+      .take(limit)
+      .collectFirst{ case Some(x) => x}
+      .getOrElse(throw new RuntimeException(s"Didn't get one after $limit tries"))
 }
 
