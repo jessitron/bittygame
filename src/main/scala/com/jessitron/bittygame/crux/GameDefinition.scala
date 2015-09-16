05b2a475e1cec6526accb8b87cf2d243338f8214
Jessica Kerr <jessitron@outpace.com>
2015-09-16 02:51:35 +0000

    first attempt doesn't implement it

--- src/main/scala/com/jessitron/bittygame/crux/GameDefinition.scala
+++ src/main/scala/com/jessitron/bittygame/crux/GameDefinition.scala
@@ -3,12 +3,12 @@ package com.jessitron.bittygame.crux
 import WhatHappens.thisHappens
 
 case class PlayerAction(trigger: Trigger, results: WhatHappens, conditions: Seq[ActionCondition]) {
-  def available(gameState: GameState): Boolean = true
+  def available(gameState: GameState): Boolean = conditions.forall(ActionCondition.met(_, gameState))
+  def conflictsWith(other: PlayerAction) = other.trigger == trigger
+
   def triggeredBy(str: String) = trigger.equalsIgnoreCase(str)
   def andProvides(item: Item) = copy(results = results.and(Acquire(item)))
   def onlyIf(condition: ActionCondition) = copy(conditions = conditions :+ condition)
-
-  def conflictsWith(other: PlayerAction) = other.trigger == trigger
 }
 object PlayerAction {
   def printing(trigger: Trigger, printedResponse: MessageToThePlayer) =
