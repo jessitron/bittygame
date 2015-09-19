dea50c139f22f7de308d1af80d996942252e2fc5
Jessica Kerr <jessitron@outpace.com>
2015-09-19 01:38:35 +0000

    widen the test (this found a bug)

--- src/test/scala/com/jessitron/bittygame/web/BittyGameServiceSpec.scala
+++ src/test/scala/com/jessitron/bittygame/web/BittyGameServiceSpec.scala
@@ -1,6 +1,6 @@
 package com.jessitron.bittygame.web
 
-import com.jessitron.bittygame.crux.{MessageToThePlayer, Opportunity, Print, Scenario}
+import com.jessitron.bittygame.crux._
 import com.jessitron.bittygame.web.messages.GameResponse
 import org.scalatest.ShouldMatchers
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
@@ -17,15 +17,19 @@ class BittyGameServiceSpec extends org.scalatest.PropSpec
 
   property ("the first turn prints the welcome message") {
     forAll(possibilitiesGen,
-           welcomeMessageGen)
+           welcomeMessageGen,
+           scenarioTitleGen)
     { (someActions: Seq[Opportunity],
-       message: MessageToThePlayer) =>
-      val someGame = Scenario("yolo", Seq(), message)
-      Put("/scenario/yolo", someGame) ~> myRoute ~> check {
+       message: MessageToThePlayer,
+        title: ScenarioTitle) =>
+
+      val someGame = Scenario(title, someActions, message)
+
+      Put("/scenario/" + encode(title), someGame) ~> myRoute ~> check {
         status should be(Created)
       }
 
-      Get("/scenario/yolo/begin") ~> myRoute ~> check {
+      Get("/scenario/" + encode(title) + "/begin") ~> myRoute ~> check {
         responseAs[GameResponse].instructions should contain(Print(message))
       }
     }
