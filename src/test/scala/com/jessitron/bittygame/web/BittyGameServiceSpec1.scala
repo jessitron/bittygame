a13d540aeda7e9b42eb149b28eb93c2da37cb5cd
Jessica Kerr <jessitron@outpace.com>
2015-09-20 18:41:04 +0000

    Example based starting point

--- src/test/scala/com/jessitron/bittygame/web/BittyGameServiceSpec1.scala
+++ src/test/scala/com/jessitron/bittygame/web/BittyGameServiceSpec1.scala
@@ -1,38 +1,30 @@
 package com.jessitron.bittygame.web
 
 import com.jessitron.bittygame.crux.{Scenario, Print}
-import com.jessitron.bittygame.gen.ScenarioGen
 import com.jessitron.bittygame.web.messages.GameResponse
-import org.scalacheck.Prop
 import org.scalatest.ShouldMatchers
-import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import spray.http._
 import spray.httpx.SprayJsonSupport._
 import com.jessitron.bittygame.serialization._
 import spray.json.DefaultJsonProtocol._
 import StatusCodes._
 
-class BittyGameServiceSpec1 extends org.scalatest.PropSpec
-with GeneratorDrivenPropertyChecks
+class BittyGameServiceSpec1 extends org.scalatest.FunSpec
 with ShouldMatchers
-with BittyGameServiceInfrastructure
-with ScenarioGen {
-   property("it prints the welcome message") {
-     forAll { (welcome: String,
-               scenario: Scenario) =>
-       whenever(welcome.nonEmpty) {
+with BittyGameServiceInfrastructure {
 
-         val someScenario = scenario.copy(welcome = welcome)
+  describe ("the first turn") {
 
-         Put("/scenario/", someScenario) ~> myRoute ~> check {
-           status should be(Created)
-         }
+    it("prints the welcome message") {
 
-         Get("/scenario/" + encode(someScenario.title) + "/begin") ~> myRoute ~> check {
-           responseAs[GameResponse].instructions should contain(Print(welcome))
-         }
-         println("it wnet")
-       }
-     }
-   }
+      val someScenario = Scenario("yoto", Seq(), "Why hello there")
+      Put("/scenario/", someScenario) ~> myRoute ~> check {
+        status should be(Created)
+      }
+
+      Get("/scenario/yoto/begin") ~> myRoute ~> check {
+        responseAs[GameResponse].instructions should contain(Print("Why hello there"))
+      }
+    }
+  }
 }
