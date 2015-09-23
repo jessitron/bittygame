fe9d34992469daa09bb45a261eee7f6f3d8b711d
Jessica Kerr <jessitron@outpace.com>
2015-09-23 18:49:10 +0000

    Reset to example

--- src/test/scala/com/jessitron/bittygame/web/BittyGameServiceSpec1.scala
+++ src/test/scala/com/jessitron/bittygame/web/BittyGameServiceSpec1.scala
@@ -1,12 +1,8 @@
 package com.jessitron.bittygame.web
 
 import com.jessitron.bittygame.crux.{Scenario, Print}
-import com.jessitron.bittygame.gen.ScenarioGen
-import com.jessitron.bittygame.scenarios.JessLife
 import com.jessitron.bittygame.web.messages.GameResponse
 import org.scalatest.ShouldMatchers
-import org.scalatest.exceptions.GeneratorDrivenPropertyCheckFailedException
-import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import spray.http._
 import spray.httpx.SprayJsonSupport._
 import com.jessitron.bittygame.serialization._
@@ -15,31 +11,24 @@ import StatusCodes._
 
 class BittyGameServiceSpec1 extends org.scalatest.FunSpec
 with ShouldMatchers
-with BittyGameServiceInfrastructure
-with GeneratorDrivenPropertyChecks
-with ScenarioGen {
+with BittyGameServiceInfrastructure {
 
   describe ("the first turn") {
 
     it("prints the welcome message") {
 
-      val message: String = "Yo Yo Yo!"
-      val temp = JessLife.scenario
-      assertCreateAndBegin(message, temp)
-    }
-  }
+      val someScenario = Scenario(title = "yoto",
+                                  welcome = "Why hello there",
+                                  opportunities = Seq(),
+                                  stats = Seq(),
+                                  items = Seq())
+      Put("/scenario/", someScenario) ~> myRoute ~> check {
+        status should be(Created)
+      }
 
-  forAll(welcomeMessageGen, scenarioGen) (assertCreateAndBegin)
-
-  def assertCreateAndBegin(message: String, temp: Scenario): Unit = {
-    val someScenario = temp.copy(welcome = message)
-
-    Put("/scenario/", someScenario) ~> myRoute ~> check {
-      status should be(Created)
-    }
-
-    Get("/scenario/" + encode(someScenario.title) + "/begin") ~> myRoute ~> check {
-      responseAs[GameResponse].instructions should contain(Print(message))
+      Get("/scenario/yoto/begin") ~> myRoute ~> check {
+        responseAs[GameResponse].instructions should contain(Print("Why hello there"))
+      }
     }
   }
 }
